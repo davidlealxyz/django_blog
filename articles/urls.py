b0a5ce74ad6137c377055a1b8b89148dbd52673d
jlcamilo03 <juancamileal@gmail.com>
2021-08-21 10:45:23 -0500

    login_requierd

--- articles/urls.py
+++ articles/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path 
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 from . import views 
 
 urlpatterns= [
-	path('home', views.ArticleListView.as_view() , name='home'),
-	path('article/<int:pk>', views.ArticleDetailView.as_view(), name='detail'),
-	
-	path('',LoginView.as_view(template_name='login.html'), name="login"),
-	 
+	path('',login_required (views.ArticleListView.as_view()), name= 'home'),
+	path('article/<int:pk>',login_required(views.ArticleDetailView.as_view()), name= 'detail'),
+		 
 ]
